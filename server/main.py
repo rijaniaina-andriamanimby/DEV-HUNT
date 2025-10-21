36b7a1a2543a6373cf8a9a6cf88e98699a593db2
rijaniaina-andriamandimby <rija.fam@gmail.com>
2025-10-21 11:15:16 +0300

    main

--- server/main.py
+++ server/main.py
@@ -6,8 +6,22 @@ import io
 import wave
 import os
 import ffmpeg
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model = whisper.load_model("base", device=device)
